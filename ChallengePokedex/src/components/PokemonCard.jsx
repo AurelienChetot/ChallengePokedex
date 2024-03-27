9dbf339dc9bfa3d5833d5b8703fe1363ba71ceec
AurelienChetot <aurelien.chetot@gmail.com>
2024-03-27 18:10:48 +0100

    PokemonCard dynamique

--- ChallengePokedex/src/components/PokemonCard.jsx
+++ ChallengePokedex/src/components/PokemonCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
         <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-        {/* Contenu de la carte Pokémon */}
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
