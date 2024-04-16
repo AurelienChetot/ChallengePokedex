abf9d0c672231e60a16bf74a5abd4042b5f7df13
AurelienChetot <161326616+AurelienChetot@users.noreply.github.com>
2024-04-16 10:58:17 +0200

    Update NavBar.jsx

--- ChallengePokedex/src/components/NavBar.jsx
+++ ChallengePokedex/src/components/NavBar.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function NavBar({ onClickPrev, onClickNext, showPrev, showNext }) {
   return (
     <div>
-      {/* Rendu conditionnel pour n'afficher le bouton "précédent" que si showPrev est vrai */}
       {showPrev && <button onClick={onClickPrev}>Précédent</button>}
-      {/* Rendu conditionnel pour n'afficher le bouton "suivant" que si showNext est vrai */}
       {showNext && <button onClick={onClickNext}>Suivant</button>}
     </div>
   );
